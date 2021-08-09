0cbaf31447d059e11ed220348c06da787627815a
Henadzi <68184678+oreshkevich@users.noreply.github.com>
2021-08-09 20:53:00 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -38,7 +38,25 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+   const re = /\B(?=(\d{10})+(?!\d))/g;
+  const animal = expr.toString().replace(re, ',');
+  const ddd = animal.replace(/10/g, '.');
+  const nnn = ddd.replace(/11/g, '-');
+  const letter = nnn.replace(/0/g, '');
+  const mew = letter.replace(/,/g, ' ');
+  const cat = mew.replace(/\*\*\*\*\*\*\*\*\*\*/g, '  ');
+  let words = (cat).split('  ');
+  let letters = words.map((w) => w.split(' '));
+  let decoded = [];
+  for(let i = 0; i < letters.length; i++){
+    decoded[i] = [];
+    for(let x = 0; x < letters[i].length; x++){
+        if(MORSE_TABLE[letters[i][x]]){
+            decoded[i].push( MORSE_TABLE[letters[i][x]] );
+        }
+    }
+  }
+  return decoded.map(arr => arr.join('')).join(' ');
 }
 
 module.exports = {
